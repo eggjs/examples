c7545745318513e5ac2a35a4f6b6e4cf72d14880
monkindey <monkindey@163.com>
2017-05-27 20:53:07 +0800

    add the test

--- download/app/controller/index.js
+++ download/app/controller/index.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 module.exports = app => {
 	class IndexController extends app.Controller {
 		* index() {
-			this.ctx.body = '<a href="/download">download</a>'
+			this.ctx.body = '<a download href="/download">download</a>'
 		}
 
 		* download(ctx) {
 			const filePath = path.resolve(__dirname, '..', 'public/egg.svg');
 			const buf = fs.readFileSync(filePath, 'utf-8');
-			ctx.set('Content-Type', 'application/octet-stream');
 			ctx.attachment('egg.svg');
+			ctx.set('Content-Type', 'application/octet-stream');
 			ctx.body = buf;
 		}
 	}
---
+++ download/test/index.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+const mm = require('egg-mock');
+const request = require('supertest');
+const svg = fs.readFileSync(
+	path.resolve(__dirname, '..', 'app/public/egg.svg')
+);
+
+describe('example download test', () => {
+	let app;
+	let server;
+
+	before(function*() {
+		app = mm.app();
+		yield app.ready();
+		server = app.listen();
+	});
+
+	after(() => app.close());
+
+	it('should GET / and show download anchor', () => {
+		return request(server)
+			.get('/')
+			.expect(200)
+			.expect(/<a download href="\/download">download<\/a>/);
+	});
+
+	it('should GET /download and download egg.svg', () => {
+		return request(server)
+			.get('/download')
+			.expect('Content-Type', 'application/octet-stream')
+			.expect('Content-Disposition', 'attachment; filename="egg.svg"')
+			.expect(200)
+			.expect(res => {
+				assert.deepStrictEqual(svg.toString(), res.text);
+			});
+	});
+});
