981e46716c3608fe11f1abede7e5b0b803fd06f1
Qi Yu <njuyuqi@gmail.com>
2017-07-19 15:23:05 +0800

    refact: use co.wrap to write migration

--- sequelize-example/migrations/20170601073501-init-migration.js
+++ sequelize-example/migrations/20170601073501-init-migration.js
@@ -1,44 +1,45 @@
 'use strict';
+const co = require('co');
 
 module.exports = {
-  up(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', {
+  up: co.wrap(function* (db, Sequelize) {
+    const { INTEGER, DATE, STRING } = Sequelize;
+
+    yield db.createTable('users', {
       id: {
-        type: Sequelize.INTEGER,
+        type: INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      name: Sequelize.STRING(30),
-      age: Sequelize.INTEGER,
-      created_at: Sequelize.DATE,
-      updated_at: Sequelize.DATE,
-    }).then(() => {
-      return queryInterface.createTable('posts', {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        title: Sequelize.STRING(30),
-        content: Sequelize.STRING(255),
-        user_id: {
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'users',
-            key: 'id',
-          },
-          onUpdate: 'cascade',
-          onDelete: 'cascade',
-        },
-        created_at: Sequelize.DATE,
-        updated_at: Sequelize.DATE,
-      });
+      name: STRING(30),
+      age: INTEGER,
+      created_at: DATE,
+      updated_at: DATE,
     });
-  },
+    yield db.createTable('posts', {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: STRING(30),
+      content: STRING(255),
+      user_id: {
+        type: INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      },
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  }),
 
-  down(queryInterface) {
-    return queryInterface.dropTable('posts').then(() => {
-      queryInterface.dropTable('users');
-    });
-  },
+  down: co.wrap(function* (db) {
+    yield db.dropTable('posts');
+    yield db.dropTable('users');
+  }),
 };
