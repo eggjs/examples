d7dbb2f21dd267efb2a0155e5b3c1a3fff0aae92
waiting <waiting@xiaozhong.biz>
2017-08-31 19:19:16 -0500

    fix: export class of DefaultConfig (#38)
    tsc throw errors when tsconfig.json set "declaration": true
    
    config/defaultConfig.ts(9,1): error TS4082: Default export of the module has or is using private name 'DefaultConfig'.
    config/defaultConfig.ts(13,28): error TS4033: Property 'config' of exported interface has or is using private name 'DefaultConfig'.
    
    define interface NewsItem as return type of HackerNews.getItem() for types recognition
    
    app/controller/news.ts: variable newsList can be recognized as type of NewsItem[] correctly instead of an object missing array

--- hackernews-async-ts/app/router.ts
+++ hackernews-async-ts/app/router.ts
@@ -1,5 +1,6 @@
 import { Application } from 'egg';
-module.exports = (app: Application) => {
+
+export default (app: Application) => {
   const controller = app.controller;
   app.redirect('/', '/news');
   app.get('/news', controller.news.list);
