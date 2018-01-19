69e994c6a1016bd909362f663ed17e49ef8f6ee8
OnedayLiu <onedayliu552@gmail.com>
2018-01-19 11:27:23 +0800

    fix: 移除 user.id 的错误展示

--- passport/app.js
+++ passport/app.js
@@ -2,7 +2,6 @@
 module.exports = app => {
   app.passport.verify(async (ctx, user) => {
     user.photo = user.photo || 'https://zos.alipayobjects.com/rmsportal/JFKAMfmPehWfhBPdCjrw.svg';
-    user.id = user.provider;
     user.displayName = user.displayName || user.name;
     return user;
   });
