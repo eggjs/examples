b206911458dfe19914ae8f88d4e9998a8fe3ee4c
popomore <sakura9515@gmail.com>
2018-02-28 15:58:23 +0800

    f

--- httpclient/app/controller/httpclient.js
+++ httpclient/app/controller/httpclient.js
@@ -68,7 +68,7 @@ class HttpclientController extends Controller {
   async home() {
     const ctx = this.ctx;
     // 示例：请求一个 npm 模块信息
-    const result = await ctx.curl('https://registry.npm.taobao.org/egg/latest', {
+    const result = await ctx.curl('https://registry.npmjs.com/egg/latest', {
       // 自动解析 JSON response
       dataType: 'json',
       // 3秒超时
--- unittest/app.js
+++ unittest/app.js
@@ -3,8 +3,8 @@
 module.exports = app => {
 
   app.beforeStart(async () => {
-    // 示例：启动的时候去读取 https://registry.npm.taobao.org/egg/latest 的版本信息
-    const result = await app.curl('https://registry.npm.taobao.org/egg/latest', {
+    // 示例：启动的时候去读取 https://registry.npmjs.com/egg/latest 的版本信息
+    const result = await app.curl('https://registry.npmjs.com/egg/latest', {
       dataType: 'json',
     });
     app.logger.info('egg latest version: %s', result.data.version);
