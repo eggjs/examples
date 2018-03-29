5be0add949acbc461c7efa7d3ecdb2db3702fafa
TZ <atian25@qq.com>
2018-03-29 11:54:28 +0800

    feat: update to new egg-bin

--- hackernews-async-ts/app/router.ts
+++ hackernews-async-ts/app/router.ts
@@ -1,9 +1,10 @@
 import { Application } from 'egg';
 
 export default (app: Application) => {
-  const controller = app.controller;
-  app.redirect('/', '/news');
-  app.router.get('/news', controller.news.list);
-  app.router.get('/news/item/:id', controller.news.detail);
-  app.router.get('/news/user/:id', controller.news.user);
+  const { controller, router } = app;
+
+  // router.redirect('/', '/news');
+  router.get('/news', controller.news.list);
+  router.get('/news/item/:id', controller.news.detail);
+  router.get('/news/user/:id', controller.news.user);
 };
---
+++ hackernews-async-ts/typings/app/service/index.d.ts
@@ -0,0 +1,10 @@
+// This file was auto created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+
+import HackerNews from '../../../app/service/HackerNews';
+
+declare module 'egg' {
+  interface IService {
+    hackerNews: HackerNews;
+  }
+}
