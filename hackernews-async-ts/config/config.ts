aa318fd5e5b7861933cc93fc4474d6093d71cffa
wanghx <whx89768@alibaba-inc.com>
2018-04-01 13:45:10 +0800

    feat: change to returnType

--- hackernews-async-ts/config/config.ts
+++ hackernews-async-ts/config/config.ts
@@ -3,24 +3,30 @@ import { EggAppConfig } from 'egg';
 import * as fs from 'fs';
 import * as path from 'path';
 import 'source-map-support/register';
-import defaultConfig from './defaultConfig';
 
-export default (appInfo: EggAppConfig) => {
-  const config: any = {};
-
-  // should change to your own
-  config.keys = appInfo.name + '123456';
-
-  config.siteFile = {
-    '/favicon.ico': fs.readFileSync(path.join(appInfo.baseDir, 'app/public/favicon.png')),
-  };
-
-  config.view = {
-    defaultViewEngine: 'nunjucks',
-    mapping: {
-      '.tpl': 'nunjucks',
+export default function config(appInfo: EggAppConfig) {
+  return {
+    keys: appInfo.name + '123456',
+    siteFile: {
+      '/favicon.ico': fs.readFileSync(
+        path.join(appInfo.baseDir, 'app/public/favicon.png'),
+      ),
+    },
+    view: {
+      defaultViewEngine: 'nunjucks',
+      mapping: {
+        '.tpl': 'nunjucks',
+      },
+    },
+    news: {
+      pageSize: 30,
+      serverUrl: 'https://hacker-news.firebaseio.com/v0',
     },
   };
-
-  return { ...config, ...defaultConfig };
 };
+
+declare module 'egg' {
+  interface Application {
+    config: EggAppConfig & ReturnType<typeof config>;
+  }
+}
