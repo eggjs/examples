a3fe1c327d9185adc3292271c42a5ceffef5e599
wanghx <whx89768@alibaba-inc.com>
2018-04-03 16:53:50 +0800

    feat: update powerpartical

--- hackernews-async-ts/typings/index.d.ts
+++ hackernews-async-ts/typings/index.d.ts
@@ -3,12 +3,10 @@ import { EggAppConfig } from 'egg';
 // extend egg
 declare module 'egg' {
   type PowerPartial<T> = {
-    [U in keyof T]?: {
-      [V in keyof T[U]]?: {
-        [W in keyof T[U][V]]?: T[U][V][W];
-      }
-    }
-  }
+    [U in keyof T]?: T[U] extends {}
+      ? { [V in keyof T[U]]?: T[U][V] extends {} ? Partial<T[U][V]> : T[U][V] }
+      : T[U]
+  };
 
   type EggConfig = PowerPartial<EggAppConfig>;
 }
