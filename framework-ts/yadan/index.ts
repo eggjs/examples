9dbcd6eb29f8da1c2194b9c8b84e4315e80366f9
waiting <1661926154@qq.com>
2018-04-09 19:53:02 +0800

    chore: update export

--- framework-ts/yadan/index.ts
+++ framework-ts/yadan/index.ts
@@ -1,32 +1,13 @@
-import {
-  AgentWorkerLoader,
-  AppWorkerLoader,
-  BaseContextClass,
-  Controller as EggController,
-  Service,
-  Subscription,
-} from 'egg'
-// import * as egg from 'egg'
+import * as egg from 'egg'
 
 import Agent from './lib/agent'
 import Application from './lib/application'
 import startCluster from './lib/cluster'
 
-// export = {
-//   ...egg,
-//   Agent,
-//   Application,
-//   startCluster,
-// }
-
-export {
-  startCluster,
-  Application,
+const newEgg = {
+  ...egg,
   Agent,
-  AppWorkerLoader,
-  AgentWorkerLoader,
-  EggController as Controller,
-  Service,
-  Subscription,
-  BaseContextClass,
+  Application,
+  startCluster,
 }
+export = newEgg
