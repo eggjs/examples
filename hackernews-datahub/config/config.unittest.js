830ba785b526ecc240fde210b5d2db27948a70d0
ziv <zivyangll@gmail.com>
2018-05-23 23:58:28 +0800

    docs: fix annotation

--- hackernews-datahub/config/config.unittest.js
+++ hackernews-datahub/config/config.unittest.js
@@ -4,7 +4,7 @@ module.exports = () => {
   const config = {};
 
   config.news = {
-    getTopStories: 'http://localhost:5678/data/hackernews/getTopStories', // datahub 路径
+    getTopStories: 'http://localhost:5678/data/hackernews/getTopStories', // datahub api path
   };
 
   return config;
