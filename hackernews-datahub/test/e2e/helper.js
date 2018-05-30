813580e597dc7adc07c5a75344a90ff80e30cda9
xudafeng <xudafeng@126.com>
2018-05-30 15:34:11 +0800

    feat: update macaca-wd

--- hackernews-datahub/test/e2e/helper.js
+++ hackernews-datahub/test/e2e/helper.js
@@ -16,15 +16,4 @@ exports.driver = wd.promiseChainRemote({
   port: process.env.MACACA_SERVER_PORT || 3456,
 });
 
-const shouldContains = (...substrs) => str => {
-  substrs.forEach(substr => assert.include(str, substr));
-};
-
-wd.addPromiseChainMethod('hasText', function(cssSelector, ...texts) {
-  return this
-    .elementByCss(cssSelector)
-    .text()
-    .then(shouldContains(...texts));
-});
-
 exports.BASE_URL = 'http://127.0.0.1:7001/';
