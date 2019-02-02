d80c40b09639062d6d8916747cef28a49f16fbe1
TZ <atian25@qq.com>
2019-02-02 13:01:57 +0800

    feat: add body-parser example

---
+++ bodyParser/app/controller/home.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const { xml2js } = require('xml-js');
+
+class HomeController extends Controller {
+  async body() {
+    const { ctx } = this;
+
+    ctx.body = {
+      type: ctx.get('content-type'),
+      body: ctx.request.body,
+      raw: ctx.request.rawBody,
+    };
+  }
+
+  async xml() {
+    const { ctx } = this;
+
+    const xmlContent = xml2js(ctx.request.body);
+    const body = xmlContent.elements[0].attributes;
+
+    ctx.body = {
+      type: ctx.get('content-type'),
+      body,
+      raw: ctx.request.rawBody,
+    };
+  }
+}
+
+module.exports = HomeController;
